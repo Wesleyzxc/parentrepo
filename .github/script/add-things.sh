558e3706a9c37aa2826b1496ea67fa288ef4d8d3
Wesleyzxc <wesleykok-@hotmail.com>
2022-07-26 20:28:57 +1000

    config git email and username

--- .github/script/add-things.sh
+++ .github/script/add-things.sh
@@ -5,6 +5,9 @@ echo "$1" >> ./test.txt
 
 echo "Finished adding hash to file"
 
+git config --global user.email "test@hotmail.com"
+git config --global user.name "test noreply"
+
 git checkout -b "update/$1"
 
 git commit -am "update"
